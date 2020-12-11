e865d0591ef62bf599a145952f59ee528778a9d1
RalfNieuwenhuizen <ralfnieuwenhuizen@gmail.com>
2020-12-11 17:22:08 +0100

    Fix compile script - print emoji as unicode

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,31 +1,34 @@
-import _ from 'lodash';
-import fs from 'fs';
+import _ from "lodash";
+import fs from "fs";
 
 import {
-    category,
-    categoryIndexMap,
-    emojiLib,
-    emojiMap,
-    emojiArray
-} from '../src/emoji-data';
+  category,
+  categoryIndexMap,
+  emojiLib,
+  emojiMap,
+  emojiArray,
+} from "../src/emoji-data";
 
 let data = {
-    category,
-    categoryIndexMap,
-    emojiLib,
-    emojiMap,
-    emojiArray
+  category,
+  categoryIndexMap,
+  emojiLib,
+  emojiMap,
+  emojiArray,
 };
 
-var stingified = JSON.stringify(data).replace(
-    /(["'])require(?:(?=(\\?))\2.)*?\1/g,
-    value => value.replace(/"/g, '')
-);
+var stingified = JSON.stringify(data)
+  .replace(/(["'])require(?:(?=(\\?))\2.)*?\1/g, (value) =>
+    value.replace(/"/g, "")
+  )
+  .replace(/[\u007F-\uFFFF]/g, function (chr) {
+    return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4);
+  });
 
 fs.writeFile(
-    'src/emoji-data/compiled.js',
-    `module.exports = ${stingified}`,
-    err => {
-        if (err) throw err;
-    }
+  "src/emoji-data/compiled.js",
+  `module.exports = ${stingified}`,
+  (err) => {
+    if (err) throw err;
+  }
 );
